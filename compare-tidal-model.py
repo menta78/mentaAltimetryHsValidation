3845245cf0e683239bd05da7bc2e8d2886f7a8be
ggarcias <ggarciasanchz@gmail.com>
2022-09-15 18:09:48 +0200

    compare signals

---
+++ compare-tidal-model.py
@@ -0,0 +1,110 @@
+import os, re
+from datetime import datetime
+import numpy as np
+import h5py
+
+from src.interpolateModelToTidelGauge import interpolateModelToTidalGauge_schismWWM
+from src.computeTidalStats import elaborateMeasures
+from src.plotStatsTidals import elaborateMeasuresPlot
+from src.plotTimeSeries import timeSeriesPlot
+
+
+def get_serie_gesla(fileName):
+    f = h5py.File(fileName,'r')
+    data = f.get("GESELD")
+
+    npoints = data["residual"].shape[0] # number of stations
+
+    res   = []
+    time  = []
+    lon   = []
+    lat   = []
+
+    for i in range(npoints):
+        ref = f["GESELD"]["longitude"][i][0]
+        lon.append(f[ref][0][0])
+        ref = f["GESELD"]["latitude"][i][0]
+        lat.append(f[ref][0][0])
+        ref = f["GESELD"]["residual"][i][0]
+        res.append(np.array(f[ref][0]))
+        ref = f["GESELD"]["time"][i][0]
+        time.append(np.array(f[ref][0]))
+    return lon, lat, res, time
+
+def getMeanEachTidal(fileName):
+    _, lat, res, _ = get_serie_gesla(fileName)
+    meanEachTidal = np.zeros(len(lat))
+    i = 0
+    for resTidal in res:
+        meanEachTidal[i] = np.nanmean(resTidal)
+        i += 1
+
+    return meanEachTidal
+
+
+# Directory where the raw globwave files are located
+# rawSatDataDir = "/home/ggarcia/Projects/mentaAltimetryHsValidation/satData/rawData"
+
+rootDir = "/eos/jeodpp/data/projects/CLIMEX/mentaAltimetryHsValidation"
+
+# Directory where the coarsened satellite data are located
+# the coarsening is performed by the coarsenSatData function. If you already performed this operation you don't need to repeat it
+tidalGaugeDataDir = os.path.join(rootDir, "data/tidalGaugeData")
+
+# Directory where the model nc files are located
+modelNcFilesDir = os.path.join(rootDir, "data/schismwwm")
+
+# Directory where the pairs observation/model are to be generated
+hsModelAndSatObsDir = os.path.join(rootDir, "data/TidalModelPairs/")
+
+# Directory where the stats are generated
+statsDir = os.path.join(rootDir, "data/stats")
+#statsDir = "../../experiments/jrc/stats/"
+
+# Directory to Tidal Gauge
+pathname = os.path.join(tidalGaugeDataDir, "GESLAv1_withResiduals.mat")
+
+meanFileTidals = os.path.join(tidalGaugeDataDir, "meanResiduals.npy")
+meanModelFile = os.path.join(rootDir, "data/elev/elevmean.nc")
+
+
+# time interval
+startDate, endDate = datetime(2002, 3, 22), datetime(2002, 12, 30)
+overwriteExisting = False
+
+# number of processes to be used for the interpolation
+nParWorker = 32
+
+# Percentile
+pth = 95
+
+# threshold above which hs should be considered
+filterSshMaximum = 100
+filterHighSsh = True
+
+# set this if you need to limit your analysis to a subdomain
+boundaries = None
+
+
+doGetMeanTidal = False
+if doGetMeanTidal:
+    meanEachTidal = getMeanEachTidal(pathname)
+    print(meanEachTidal.shape[:])
+    np.save(meanFileTidals, meanEachTidal)
+
+
+lonInterest = -4.01
+latInterest = 48.73
+target = [lonInterest, latInterest]
+
+timeSeriesPlot(
+    startDate,
+    endDate,
+    hsModelAndSatObsDir,
+    statsDir,
+    target,
+    meanFileTidals,
+    meanModelFile,
+    pth = pth,
+) 
+
---
+++ src/plotTimeSeries.py
@@ -0,0 +1,115 @@
+import os, re, glob
+import numpy as np
+from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
+from matplotlib.colorbar import Colorbar
+import matplotlib.gridspec as gridspec
+import itertools
+from mpl_toolkits.basemap import Basemap
+
+
+filterHighSsh = False
+options_savefig = {'dpi': 150, "bbox_inches": "tight", "transparent": False}
+title_font = {'size':'12', 'color':'black', 'weight':'normal',
+                      'verticalalignment':'bottom'}
+
+
+def find_by_index(array, value):
+    index = np.where(array = value)
+    return index
+
+
+def getFiles(hsSatAndModelDir, startDate, endDate):
+    fl = []
+
+    while startDate <= endDate:
+        strtime = startDate.strftime("%Y%m%d")
+        pthfile = hsSatAndModelDir + "/ERA5_schismwwm_"+strtime+"*.npy"
+        fileFound = glob.glob(pthfile)
+        fl.append(fileFound)
+        startDate += timedelta(days=1)
+
+    return list(itertools.chain(*fl))
+
+
+def timeSeriesPlot(
+    startDate,
+    endDate,
+    hsSatAndModelDir,
+    outputDir,
+    target,
+    meanFileTidals,
+    meanModelFile,
+    pth = 90,
+):
+
+    def loadFile(flpth):
+        print("")
+        print("    loading file " + flpth)
+        satdts = np.load(flpth)
+        if filterHighSsh:
+            sshsat = satdts[:, 0]
+            sshmdl = satdts[:, 1]
+            cnd = np.logical_and(
+                sshsat < filterSshMaximum, sshmdl < filterSshMaximum
+            )
+            satdts = satdts[cnd, :]
+        return satdts
+
+    fls = getFiles(hsSatAndModelDir, startDate, endDate)
+
+    obs = np.array([])
+    model = np.array([])
+    Lonn = model
+    Latt = model
+    Indx = model
+
+    for f in fls:
+        data_ = loadFile(f)
+        obs_ = data_[:,0]
+        model_ = data_[:,1]
+        repLon = data_[:,2]
+        repLat = data_[:,3]
+        repIdx = data_[:,4] 
+
+        obs = np.concatenate([obs, obs_])
+        model = np.concatenate([model, model_])
+        Lonn = np.concatenate([Lonn, repLon])
+        Latt = np.concatenate([Latt, repLat])
+        Indx = np.concatenate([Indx, repIdx])
+        
+    # load mean of each tidal
+    print(meanFileTidals)
+    meanTidals = np.load(meanFileTidals)
+
+    # load mean of each node in the model
+    meanModels = 0
+    
+    uniqueIdx, jIdx = np.unique(Indx[np.isfinite(Indx)], return_index=True)
+
+    meanModel = np.nanmean(model)
+    model = model - meanModel
+  
+    pmodel = np.nanpercentile(model, pth)
+    pobs = np.nanpercentile(obs, pth)
+
+    nseT = []
+    r2T = []
+    absreT = []
+    reT = []
+    uniqueLon = []
+    uniqueLat = []    
+
+
+    index = find_by_index(Lonn, target[0])
+    index = find_by_index(Lonn, target[0])
+    obsTarget   = obs[index]
+    modelTarget = model[index]
+
+
+    fig, ax = plt.subplots()
+    # It's missing time array
+    plt.plot(obsTarget)
+    plt.plot(modelTarget)
+    plt.savefig('~/Desktop/tidalVSmodel_lon='+str(lon)+'_lat='+str(lat) + ".png", **options_savefig)
+    plt.close(fig)
